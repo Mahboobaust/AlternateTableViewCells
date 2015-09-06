72a143b70aa803a188ebb9d761e3b2d71756fc68
Mahboobaust <mahboobaust13@gmail.com>
2015-09-06 14:17:34 +0600

    Initial Commit

---
+++ AlternateTableViewCells/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AlternateTableViewCells
+//
+//  Created by Mahboob on 9/6/15.
+//  Copyright (c) 2015 Mahboob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AlternateTableViewCells/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AlternateTableViewCells
+//
+//  Created by Mahboob on 9/6/15.
+//  Copyright (c) 2015 Mahboob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
